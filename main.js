71b7de9799ff505b3c88cb59cbc6635a1808b736
dangalcan <megamagolas@gmail.com>
2025-11-08 13:24:46 +0100

    feat: add logger and .env management

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from "url";
+import logger from "./logger.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, ".env"), quiet: true });
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-const PORT = 3000;
+
+
+
+
 
 app.listen(PORT, () => {
-  console.log(`API running at http://localhost:${PORT}`);
+  logger.warn(`Using log level: ${process.env.LOG_LEVEL}`)
+  logger.info(`Server started on port ${PORT}`);
+  logger.info(`API running at http://localhost:${PORT}`);
 });

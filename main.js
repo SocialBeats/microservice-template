cd5fceab16bd6304ad42b611863bdcafa38182d3
dangalcan <megamagolas@gmail.com>
2025-11-08 14:52:05 +0100

    feat: add initial aboutRoutes.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from "url";
 import logger from "./logger.js";
+// import your routes here 
+import aboutRoutes from "./src/routes/aboutRoutes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,9 +18,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-
-
+// add your routes here like this:
+aboutRoutes(app)
 
 app.listen(PORT, () => {
   logger.warn(`Using log level: ${process.env.LOG_LEVEL}`)
